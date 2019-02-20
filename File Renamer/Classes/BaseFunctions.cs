a144501c4d32ca49359dada6f582fa66bc31b96c
Jynsaillar <jynsaillar@ymail.com>
2019-02-20 06:34:15 +0100

    Add project files.

---
+++ File Renamer/Classes/BaseFunctions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace File_Renamer.Classes {
+    public static class BaseFunctions {
+        /// <summary>
+        ///     Returns a list of files with full path names as strings.
+        /// </summary>
+        /// <param name="dir">Root directory pointing to the location of the files to be returned.</param>
+        /// <returns></returns>
+        public static List<string> GetFiles(string dir) {
+            var files = new List<string>();
+
+            try {
+                // If a valid path parameter has been passed into the function
+                if (!string.IsNullOrEmpty(dir))
+                    if (Directory.Exists(Path.GetDirectoryName(dir)))
+                        files.AddRange(Directory.GetFiles(Path.GetDirectoryName(dir)));
+            }
+            catch (Exception E) {
+                Console.WriteLine(E.Message, "Error - no files found");
+            }
+
+            return files;
+        }
+
+        /// <summary>
+        ///     Method to trim part of a file's name for all files in a list.
+        /// </summary>
+        /// <param name="partToRemove">String to be removed.</param>
+        /// <param name="files">A list containing all valid files.</param>
+        public static void RemovePartOfFilename(string partToRemove, List<string> files) {
+            // If the list isn't empty, do...
+            if (files != null) {
+                // Counter needed to display how many invalid/non-matching files were skipped after renaming all valid files
+                var skipped = 0;
+
+                // Loop through all given files
+                foreach (var file in files)
+                    // If part of a files' name matches partToRemove: Print debug information to the console (current file path and modified file path)
+                    if (file.Contains(partToRemove)) {
+                        Console.WriteLine("Current path:\n{0}", file);
+
+                        var newPath = Path.GetDirectoryName(file) + "\\" + Path.GetFileName(file).Replace(partToRemove, "");
+
+                        Console.WriteLine("New path:\n{0}", newPath);
+
+                        File.Move(file, newPath);
+                    }
+                    // Increment counter of skipped files
+                    else {
+                        skipped++;
+                    }
+
+                // Debug feedback/confirmation
+                Console.WriteLine("\nFile count: {0}, {1} skipped -- Task complete.", files.Count, skipped);
+            }
+            else {
+                Console.WriteLine("Task failed.");
+            }
+        }
+    }
+}
---
+++ File Renamer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using File_Renamer.Classes;
+
+// DISCLAIMER: This program only works if it is called through the CMD passing the desired root directory as (first) parameter!
+namespace File_Renamer {
+    internal class Program {
+        private static void Main(string[] args) {
+            // If a file has been loaded...
+            if (args.Length > 0) {
+                Console.WriteLine("Enter the string you want to remove:");
+
+
+                //Load files and, if any, rename files using the given string
+                BaseFunctions.RemovePartOfFilename(Console.ReadLine(), BaseFunctions.GetFiles(args[0]));
+            }
+            else {
+                Console.WriteLine("Error - no files found.");
+            }
+
+            //Keep the console window open to let the user read the output state (task completed OR failed)
+            Console.ReadKey();
+        }
+    }
+}
